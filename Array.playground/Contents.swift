d6375bda3cb9a24a24b9a29b16bf0a548aff2b71
Student12 <Student12@Student12s-iMac.local>
2017-06-13 13:25:20 +0700

    StartApp

---
+++ Array.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// การใช้ตัวแปร array
+// การประกาศตัวแปร array และกำหนดค่า
+
+// implicit
+let intArrayNumber1 = [11,22,33,44]
+
+// Explicit
+var intArrayNumber2 = [Int]()
+intArrayNumber2 = [55,66,77,88]
+
+// String Type
+var strArrayName1 = ["Doramon","Nobita","มาสเตอร์"]
+var strArrayName2 = [String]()
+strArrayName2 = ["AAA","BBB","ccc"]
+
+// Double
+var douArrayName3 = [1.2, 1.3, 1.4]
+var douArrayNub4 = [Double]()
+douArrayNub4 = [2.1, 2.2, 2.3]
+
+
+
+
---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,65 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// This Comment หรือ นี่คือ comment
+// นี้คือการประกาศตัวแปร แบบ ไม่สามารถเปลี่ยนค่าได้
+let myConstant = 123 // Imlicit constant
+//myConstant = 456 cannot change let
+
+//นี้คือก่ีประกาศตัวแปรที่เปลี่ยนค่าได้
+var intNumber = 123  //Implicit variable intNumber = 123
+intNumber = 456
+
+let myConstant1: Int = 123
+
+
+
+//Display on Console
+print(intNumber) // Display only Value
+print("intNumber ==> \(intNumber)")
+
+//Explicit String การประกาศแบบ กำหนด
+//Data Type ==> String
+let strName: String = "Doramon"
+
+//Implicit String Datatype ==> String
+let strSurname = "Japane"
+
+//Operate
+var strOfficer = strName + " " + strSurname
+strOfficer = "Mr." + strOfficer
+
+//Operating on Number
+let intA = 5
+let intB = 2
+
+var answer = intA + intB
+answer = intA * intB
+answer = intA / intB
+
+// Change Datatype Integer to Double
+var douA = Double(intA)
+var douB = Double(intB)
+
+var answer2: Double = douA / douB
+
+var myStatus = true //Implicit
+var myStatus2: =Bool = false //Explicit
+
+//print on Console ==> ถ้าเอา 5 บาท 6 เท่ากับ  11
+
+//String Manage
+let strTitle1 = "ถ้าเอา"
+let strTitle2 = "เท่ากับ"
+let strAdd = "บวก"
+let strHand = "หาร"
+let strSpace = " "
+
+//Int Manage
+let intNum1 = 5
+let intNum2 = 6
+
+print(strTitle1 + strSpace + "\(intNum1)" + strSpace + strAdd + strSpace + "\(intNum2)" + strSpace + strTitle2 + strSpace + "\(intNum1 + intNum2)")
+
+
